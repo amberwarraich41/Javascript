f9c52882b1a16418104639a014b7a2dc6f6711b7
Amber Faisal <amberwarraich41@gmail.com>
2026-07-16 06:01:04 +0000

    container config done

--- basic/variable.js
+++ basic/variable.js
@@ -1,2 +1,9 @@
 let name=("Amber Faisal")
 console.log(name)
+const age = 21;
+console.log(age);
+// multiple variable 
+let qualification="BSCS";
+let marks=80;
+let university="Lahore Garrison University";
+console.table([name,age,qualification,marks,university])
